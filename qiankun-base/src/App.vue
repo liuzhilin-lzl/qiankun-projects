3ae612965246a90e96d2de1f59197e8d52219805
liuzhilin <17663969066@163.com>
2021-07-12 23:28:24 +0800

    配置

--- qiankun-base/src/App.vue
+++ qiankun-base/src/App.vue
@@ -1,33 +1,37 @@
 <template>
-  <div id="app">
-      
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+	<div id="app">
+		<!-- 可以放自己的路由,也可以放子应用 -->
+		<el-menu :router="true" mode="horizontal">
+			<el-menu-item index="/"></el-menu-item>
+			<el-menu-item index="/vue"></el-menu-item>
+			<el-menu-item index="/react"></el-menu-item>
+		</el-menu>
+		<div></div>
+		<router-view></router-view>
+		<div id="vue"></div>
+		<div id="react"></div>
+	</div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+	padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+	font-weight: bold;
+	color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
--- qiankun-base/src/main.js
+++ qiankun-base/src/main.js
@@ -1,15 +1,33 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import { registerMicroApps, start } from 'qiankun';
+
+registerMicroApps([
+	{
+		name: 'vueApp', //应用名字
+		entry: '//localhost:1000', //默认会加载这个html,解析里面的js 动态执行 (子应用必须支持跨域)
+		container: '#vue', //容器名字
+		activeRule: '/vue', //激活路径
+	},
+	{
+		name: 'reactApp',
+		entry: '//localhost:2000',
+		container: '#react',
+		activeRule: '/react',
+	},
+]);
+// 启动 qiankun
+start();
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+	router,
+	store,
+	render: (h) => h(App),
+}).$mount('#app');
